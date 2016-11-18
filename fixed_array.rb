65fa61c6f78346027fa1d9a5dbc489a0b82ac8b2
omgannie <annie_oh@icloud.com>
2016-11-18 14:45:18 -0600

    solve fixed array

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,3 +1,20 @@
 class FixedArray
+  attr_accessor :array
 
+  class IndexOutOfBoundsError < StandardError
+  end
+
+  def initialize(size)
+    @array = Array.new(size)
+  end
+
+  def get(index)
+    raise IndexOutOfBoundsError if index.to_i >= self.array.length || index < 0
+    @array[index]
+  end
+
+  def set(index, element)
+    raise IndexOutOfBoundsError if index.to_i >= self.array.length || index < 0
+    @array[index] = element
+  end
 end
--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,5 +1,30 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
-  
+  let(:my_array) { FixedArray.new(3) }
+
+  it "initializes a new FixedArray object" do
+    expect(my_array.class).to eq FixedArray
+  end
+
+  it "initializes a new object with an array of 3 empty spaces" do
+    expect(my_array.array).to eq [nil, nil, nil]
+  end
+
+  it "gets the value at an index of the array" do
+    expect(my_array.get(0)).to eq nil
+  end
+
+  it "raises an error if method tries to get from non-existing index" do
+    expect{ my_array.get(4) }.to raise_error(IndexOutOfBoundsError)
+  end
+
+  it "sets the value at an index of the array" do
+    expect(my_array.set(0, "my element")).to eq "my element"
+  end
+
+  it "raises an error if method tries to set at non-existing index" do
+    expect{ my_array.set(4, "error error") }.to raise_error(IndexOutOfBoundsError)
+  end
+
 end
